48cca74a5dd2ce6ed4ce44777b2e2fe3a14e3a4a
psoto <psoto23.ps@gmail.com>
2017-12-26 18:42:25 -0300

    Added database

--- api/views/projects.py
+++ api/views/projects.py
@@ -1,9 +1,15 @@
 from flask import Blueprint, jsonify
 
+from ..models import db, Project
+from ..serializers import ProjectSchema
+
 
 bp = Blueprint('projects', __name__, url_prefix='/projects')
 
 
 @bp.route('/', methods=['GET'])
 def project_list():
-    return jsonify(projects=[])
+    projects = db.query(Project).all()
+    schema = ProjectSchema()
+    serialized = [schema.dump(p).data for p in projects]
+    return jsonify(projects=serialized)
