3c17e579621e9d22f6fe96125981099ba2fd6ecd
Rodrigo Herrera <rhherrera@gmail.com>
2018-01-04 15:07:36 -0300

    added new models and endpoints.

--- api/models.py
+++ api/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, JSON, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import sessionmaker, scoped_session, relationship
 
 from . import settings
 
@@ -18,16 +18,32 @@ class Project(Base):
 
     __tablename__ = 'projects'
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String, nullable=False, unique=True)
+    images = relationship('Image', backref='project')
 
     def __repr__(self):
         return f'<Project({self.id}, {self.name})>'
 
 
+class Image(Base):
+    __tablename__ = 'images'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    project_id = Column(
+        Integer, ForeignKey('projects.id', ondelete='CASCADE'), nullable=True
+    )
+    name = Column(String, nullable=False)
+    tags = Column(JSON, nullable=True)
+
+    def __repr__(self):
+        return f'<Image({self.id}, {self.name})>'
+
+
 # TODO: Should be done in alembic.
 # Create the schema if it doesn't already.
 Base.metadata.create_all(engine)
+
 
 session_factory = sessionmaker(bind=engine)
 db = scoped_session(session_factory)
--- api/views/projects.py
+++ api/views/projects.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, jsonify
+import os
+from flask import Blueprint, jsonify, request
+from flask_apispec import marshal_with, use_kwargs
+from PIL import Image as PilImage
 
-from ..models import db, Project
+from ..models import db, Project, Image
 from ..serializers import ProjectSchema
 
+DEFAULT_THUMBNAIL_SIZE = (120, 120)
 
 bp = Blueprint('projects', __name__, url_prefix='/projects')
 
@@ -13,3 +17,66 @@ def project_list():
     schema = ProjectSchema()
     serialized = [schema.dump(p).data for p in projects]
     return jsonify(projects=serialized)
+
+
+@bp.route('/', methods=['POST'])
+@use_kwargs(ProjectSchema)
+def create_project(kwargs):
+    project = Project(**kwargs)
+    project.save()
+
+    target = 'upload/{}'.format(project.name)
+    os.mkdir(target)
+    thumbnails_folder = '{}/thumbnails'.format(target)
+    os.mkdir(thumbnails_folder)
+
+    return project
+
+
+@bp.route('/<id>', methods=('GET',))
+def get_project(id):
+    project = Project.query.filter_by(id=id).first()
+    return project
+
+
+@bp.route('/<id>/images', methods=['POST'])
+def upload_images(id):
+    project = Project.query.filter_by(id=id).first()
+    target = 'upload/{}'.format(project.name)
+    thumbnails_folder = '{}/thumbnails'.format(target)
+    images = []
+    for upload in request.files.getlist('file'):
+        filename = upload.filename.rsplit('/')[0]
+        destination = '/'.join([target, filename])
+        print('Accept incoming file:', filename)
+        print('Save it to:', destination)
+        upload.save(destination)
+        images.append(Image(name=filename))
+        # generate thumbnail
+        PilImage.open(destination).thumbnail(DEFAULT_THUMBNAIL_SIZE).save(
+            '/'.join([thumbnails_folder, filename]))
+    db.bulk_save_objects(images)
+    db.commit()
+    return jsonify(status='ok', msg='upload successful')
+
+
+@bp.route('/<id>/images', methods=['GET'])
+def get_images(id):
+    project = Project.query.filter_by(id=id).first()
+    return jsonify(status='ok', images=[img.name for img in project.images])
+
+
+@bp.route('/<id>/images/<imagename>', methods=['GET'])
+def get_image(id, imagename):
+    project = Project.query.filter_by(id=id).first()
+    imagePath = 'upload/{}/{}'.format(project.name, imagename)
+    return send_file(imagePath)
+
+
+@bp.route('/<id>/images/thumbnail/<imagename>', methods=['GET'])
+def get_image_thumbnail(id, imagename):
+    project = Project.query.filter_by(id=id).first()
+    target = 'upload/{}'.format(project.name)
+    thumbnails_folder = '{}/thumbnails'.format(target)
+    thumbnail_path = '{}/{}'.format(thumbnails_folder, imagename)
+    return send_file(thumbnail_path)
