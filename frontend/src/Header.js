8f23a52b09155570137c0ea61766426b83192d32
Bruno Berisso <bruno@tryolabs.com>
2018-01-26 12:50:45 +0000

    Add a menu to the project settings button

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -5,10 +5,68 @@ import AppBar from 'material-ui/AppBar'
 import Toolbar from 'material-ui/Toolbar'
 import Typography from 'material-ui/Typography'
 import Button from 'material-ui/Button'
+import Menu, { MenuItem } from 'material-ui/Menu'
 import { withStyles } from 'material-ui/styles'
 import icon from './icon.png'
 
-const styles = {
+const menuStyles = theme => ({
+  separator: {
+    marginLeft: 16,
+    fontSize: 14,
+    color: theme.palette.text.secondary,
+  },
+})
+
+class _ProjectMenuButton extends React.Component {
+  state = {
+    anchorElement: null,
+  }
+
+  handleClick = event => this.setState({ anchorElement: event.currentTarget })
+  handleClose = () => this.setState({ anchorElement: null })
+
+  render() {
+    const { classes } = this.props
+    const anchor = this.state.anchorElement
+    const ownership = anchor ? 'project-menu' : null
+
+    return (
+      <div>
+        <Button
+          aria-owns={ownership}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+        >
+          <CogIcon size={24} color="white" />
+        </Button>
+        <Menu
+          id="project-menu"
+          anchorEl={anchor}
+          open={Boolean(anchor)}
+          onClose={this.handleClose}
+        >
+          <MenuItem onClick={this.handleClose}>Upload Images</MenuItem>
+          <Typography className={classes.separator}>Tags</Typography>
+          <MenuItem onClick={this.handleClose}>Import</MenuItem>
+          <MenuItem onClick={this.handleClose}>Export</MenuItem>
+          <MenuItem onClick={this.handleClose}>Clear all</MenuItem>
+        </Menu>
+      </div>
+    )
+  }
+}
+
+_ProjectMenuButton.propTypes = {
+  classes: PropTypes.object.isRequired,
+  onUploadImage: PropTypes.func,
+  onImportTags: PropTypes.func,
+  onExportTags: PropTypes.func,
+  onClearAllTags: PropTypes.func,
+}
+
+const ProjectMenuButton = withStyles(menuStyles)(_ProjectMenuButton)
+
+const headerStyles = {
   icon: {
     maxHeight: '60%',
     maxWidth: '60%',
@@ -18,26 +76,21 @@ const styles = {
     flex: 1,
   },
   toolbar: {
-     height: 40,
+     height: 10,
   },
 };
 
 const Header = ({ classes, settingsSelected }) =>
   <AppBar>
-    <Toolbar style={styles.toolbar}>
-      <img src={icon} alt="" style={styles.icon}/>
+    <Toolbar style={headerStyles.toolbar}>
+      <img src={icon} alt="" style={headerStyles.icon}/>
       <Typography type="title" color="inherit" className={classes.title}>
         Taggerine
       </Typography>
-      <Button color="inherit" onClick={settingsSelected}>
-        <CogIcon size={24} color="white" />
-      </Button>
+      <ProjectMenuButton />
     </Toolbar>
   </AppBar>
 
-Header.propTypes = {
-  classes: PropTypes.object.isRequired,
-  settingsSelected: PropTypes.func,
-}
+Header.propTypes = ProjectMenuButton.propTypes
 
-export default withStyles(styles)(Header)
+export default withStyles(headerStyles)(Header)
